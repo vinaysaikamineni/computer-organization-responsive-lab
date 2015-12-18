6280c8e492c0a75291c12dd673b4fd088fe622f8
S Giritheja <s.giritheja@gmail.com>
2015-12-18 17:06:01 +0530

    Completed exp2 conversion

---
+++ exp2/float.js
@@ -0,0 +1,188 @@
+/*
+	Name : Floating Point Numbers Representation Simulator
+	Author : Giritheja
+	Description : This file converts the given input into three 
+	normalized representations :
+	1.No-Zero Noramlization
+	2.Abrupt underflow Normalization
+	3.Denormalization 
+	It determines whether the given output is rounded or accurate in 
+	the above three formats.
+*/
+//dictionary for the denoramlized numbers the numbers are multiplied by 1000.
+var denormalized = [ 0, 250, 500, 750, 1000, 1250, 1500, 1750, 2000, 2500, 3000, 3500];	
+//dictionary for the denoramlized numbers the numbers are multiplied by 1000.
+var normalized = [ 500, 625, 750, 875, 1000, 1250, 1500, 1750, 2000, 2500, 3000, 3500];
+//dictionary for positions of different number to place the pointing arrow on the canvas.
+var scalednorm =[62,70,78,86,94,107,124,144,160,187,223,258];
+//dictionary for positions of different number to place the pointing arrow on the canvas.
+var scaleddenorm = [48,58,75,89,104,117,134,154,170,197,233,268];
+var accuracy = document.getElementById('accuracytable');
+var c = document.getElementById('myCanvas');
+var ctx = c.getContext("2d");
+var img = new Image();   // Create new img element
+img.src = 'images/normalized.png'; // Set source path
+ctx.drawImage(img,0,0);
+ctx.beginPath(); //40px at zero	
+//ctx.moveTo(104,380); //258 at 3.5
+//ctx.lineTo(104,340);	//260 220 140 100
+//ctx.stroke();
+//function to clear all values set input values to null.
+function clearall(){
+ctx.drawImage(img,0,0);
+ctx.beginPath(); //40px at zero	
+accuracy.rows[0].cells[1].innerHTML=null;	
+accuracy.rows[0].cells[1].style.color='transparent';
+accuracy.rows[1].cells[1].innerHTML=null;	
+accuracy.rows[1].cells[1].style.color='transparent';
+accuracy.rows[2].cells[1].innerHTML=null;	
+accuracy.rows[2].cells[1].style.color='transparent';
+$('#resulttable tr').each(function() {
+  			$(this).find("td:nth-child(5)").context.cells[4].style.backgroundColor='transparent';  			
+  			$(this).find("td:nth-child(3)").context.cells[2].style.backgroundColor='transparent';
+  			$(this).find("td:nth-child(2)").context.cells[1].style.backgroundColor='transparent';
+  			$(this).find("td:nth-child(1)").context.cells[0].style.backgroundColor='transparent';
+  			$(this).find("td:nth-child(4)").context.cells[3].style.backgroundColor='transparent';		
+  			
+  			
+  	})
+}  
+ //function to draw arrows the on the no-zero normalized number line 
+ //and abrupt underflow numberline.
+  function drawarrownorm(index) {
+  	var xcord = scalednorm[index];
+  	ctx.moveTo(xcord,140);
+	ctx.lineTo(xcord,100);
+	ctx.stroke();
+	ctx.moveTo(xcord-10,110);
+	ctx.lineTo(xcord,100);
+	ctx.stroke();
+	ctx.moveTo(xcord+10,110);
+	ctx.lineTo(xcord,100);
+	ctx.stroke();
+  	if(index>3){
+  	ctx.moveTo(xcord+10,260);
+	ctx.lineTo(xcord+10,220);
+	ctx.stroke();
+  	ctx.moveTo(xcord-10+10,230);
+	ctx.lineTo(xcord+10,220);
+	ctx.stroke();
+	ctx.moveTo(xcord+10+10,230);
+	ctx.lineTo(xcord+10,220);
+	ctx.stroke();
+  	} else {
+  	ctx.moveTo(40+10,260);
+	ctx.lineTo(40+10,220);
+	ctx.stroke();
+	ctx.moveTo(40,230);
+	ctx.lineTo(40+10,220);
+	ctx.stroke();
+	ctx.moveTo(60,230);
+	ctx.lineTo(40+10,220);
+	ctx.stroke();
+  	}
+  }
+ //function to draw arrows the on the denormalized number line 
+  function drawarrowdenorm(index) {
+  	var xcord = scaleddenorm[index];
+  	ctx.moveTo(xcord,380);
+	ctx.lineTo(xcord,340);
+	ctx.stroke();
+	ctx.moveTo(xcord-10,350);
+	ctx.lineTo(xcord,340);
+	ctx.stroke();
+	ctx.moveTo(xcord+10,350);
+	ctx.lineTo(xcord,340);
+	ctx.stroke();
+  } 
+  //function to calculate the index in normalized and denormalized dictionaries
+  //by comparing which is the number to closest to and then calling drawarrow functions
+  //to draw arrows on the numberline.
+
+  function convertfloat() {
+  	clearall();
+  	var numberstr = document.getElementById('decimal').value;
+  	var number=parseInt(parseFloat(numberstr.substring(0,5))*1000);
+  	if (number < 0) {
+  		number=-number;
+  	}
+  	//To check if the input is valid.
+  	if(number>3875||isNaN(number)){
+  		return;
+  	}
+  	console.log(number);
+  	var leastdiffdenorm;
+ 	var indexdenorm=0;
+ 	var indexnorm=0;
+ 	var leastdiffnorm;
+ 	var trignorm=0;
+ 	var trigdenorm=0;
+ 	for( var i = 0 ; i < normalized.length ; i++ ) {
+ 		if(number >= normalized[i]){
+ 			if(trignorm){
+ 				if( leastdiffnorm > (number-normalized[i]) ) {
+ 					leastdiffnorm = number-normalized[i];
+ 					indexnorm=i;
+ 				}
+ 			}else {
+ 				leastdiffnorm = number-normalized[i];
+ 				indexnorm=i;
+ 				trignorm++;
+ 				
+ 			}
+ 		}
+ 		if(number >= denormalized[i]){
+ 			if(trigdenorm){
+ 				if( leastdiffdenorm > (number-denormalized[i]) ) {
+ 					leastdiffdenorm = number-denormalized[i];
+ 					indexdenorm=i;
+ 				}
+ 			}else {
+ 				leastdiffdenorm = number-denormalized[i];
+ 				indexdenorm=i;
+ 				trigdenorm++;
+ 				
+ 			}
+ 		}
+ 	}
+ 	if(parseFloat(numberstr.substring(0,5))*1000==normalized[indexnorm]){
+ 					if(indexnorm>3){
+ 						accuracy.rows[1].cells[1].innerHTML='ACCURATE';	
+ 						accuracy.rows[1].cells[1].style.color='green';
+ 						}else{
+ 						accuracy.rows[1].cells[1].innerHTML='ROUNDED';	
+ 						accuracy.rows[1].cells[1].style.color='red';	
+ 						}
+ 		accuracy.rows[0].cells[1].innerHTML='ACCURATE';	
+ 		accuracy.rows[0].cells[1].style.color='green';
+ 						
+ 	} else {
+ 		accuracy.rows[0].cells[1].innerHTML='ROUNDED';	
+ 		accuracy.rows[0].cells[1].style.color='red'; 						
+ 		accuracy.rows[1].cells[1].innerHTML='ROUNDED';	
+ 		accuracy.rows[1].cells[1].style.color='red';
+ 	}
+ 	if(parseFloat(numberstr.substring(0,5))*1000==denormalized[indexdenorm]){
+ 		accuracy.rows[2].cells[1].innerHTML='ACCURATE';	
+ 		accuracy.rows[2].cells[1].style.color='green';
+ 	} else {
+ 		accuracy.rows[2].cells[1].innerHTML='ROUNDED';	
+ 		accuracy.rows[2].cells[1].style.color='red';
+ 	}
+ 	drawarrownorm(indexnorm);
+ 	drawarrowdenorm(indexdenorm); 	
+ 	$('#resulttable tr').each(function() {
+  		if(parseFloat($(this).find("td:nth-child(5)").html())*1000==denormalized[indexdenorm]){
+  			$(this).find("td:nth-child(5)").context.cells[4].style.backgroundColor='blue';
+  		}
+  			
+  		if(parseFloat($(this).find("td:nth-child(3)").html())*1000==normalized[indexnorm]){
+  			$(this).find("td:nth-child(3)").context.cells[2].style.backgroundColor='red';
+  			$(this).find("td:nth-child(2)").context.cells[1].style.backgroundColor='red';
+  			$(this).find("td:nth-child(1)").context.cells[0].style.backgroundColor='red';
+  			$(this).find("td:nth-child(4)").context.cells[3].style.backgroundColor='red';		
+  			
+  		}	
+  	})
+
+  }
--- exp2/main.js
+++ exp2/main.js
@@ -1,17 +1,169 @@
 /*
 	Name:Floating Point Numbers Representation Simulator
+	Author:Giritheja
+	Description: This converts the given number with given number
+				 of exponent bits into Floating point representation
+				 of numbers.The input is broken into integral and 
+				 fractional part.The binary representation of each 
+				 part is found.The two parts are then combined and normalized
+				 form is found.The exponent, Mantissa, Sign and bias are also
+				 given out.
 */
-function convert(){
+document.getElementById('resultdiv').style.display='none';
+//hiding resultdiv.
+// function to clear all set variables and settings different results to null
+function clearall() {
+	document.getElementById('resultdiv').style.display='none';
+	document.getElementById('integral').value = null;
+	document.getElementById('fractional').value = null;
+	document.getElementById('binarynum').value = null;
+	document.getElementById('sign').value = null;	
+	document.getElementById('bias').value = null;	
+	document.getElementById('max').innerHTML = null;
+	document.getElementById('min').innerHTML = null;
+	document.getElementById('positiveRange').innerHTML=null;
+	document.getElementById('negativeRange').innerHTML=null;	
+	document.getElementById('normalized').value = null;
+	document.getElementById('mantiss').value=null; 
+	document.getElementById('expone').value=null;
+	document.getElementById('error0').innerHTML=null;
+	document.getElementById('error1').innerHTML=null;
+	document.getElementById('error2').innerHTML=null;				
+}
+//function to convert the input number to normalized form
+//with number of bits for exponent as constraint.
+function convert() {
+	clearall();
 	var number = parseFloat(document.getElementById('number').value);
 	var expolength = parseInt(document.getElementById('expolength').value);
-	console.log(number+expolength);
-	var fractional = number-parseInt(number);
-	var integral = parseInt(number);
-	var binarystr = number.toString(2);
-	var binarynumber = parseFloat(binarystr);
-	var binaryintegral = parseInt(binarystr); 
-	var binaryfractional = ((binarynumber-parseInt(binarystr)).toString()).substring(3,11);  
+	var sign=0;
+	//To check if the input is within constraints
+	if(expolength>6||expolength<1||isNaN(expolength)||isNaN(number)){
+		return;
+	}
+	if(number<0){
+		sign=1;
+		number=-number;
+	}
+	document.getElementById('resultdiv').style.display='block';
+	var fraclength;
+	var fractional;
+	var integral;
+	var binarystr;
+	var binarynumber;
+	var binaryintegral;
+	var binaryfractional;
+	var bias = Math.pow(2,expolength-1)-1;
+	var twopower;
+	var normalized;
+	var expone;
+	var mantiss;
+	var exponebinary;
+	var exponeunderflow=0;
+	var exponeoverflow=0;
+	var mantissaerror=0;
+	//dictionary for max representable numbers.
+	var max =[1.96875,3.875,15,224,49152,2147483648];
+	fraclength = 8-expolength;
+	binarystr = number.toString(2);
+	fractional = number-parseInt(number);
+	integral = parseInt(number);
+	binarynumber = parseFloat(binarystr);
+	binaryintegral = parseInt(binarystr);
+	binaryfractional = (binarystr).substring((binaryintegral.toString()).length+1,(binaryintegral.toString()).length+9);  	
 	document.getElementById('integral').value = binaryintegral;
 	document.getElementById('fractional').value = binaryfractional;
-	document.getElementById('binarynum').value = binaryintegral+'.'+binaryfractional;
-}
+	if(binaryfractional.length>0)
+		document.getElementById('binarynum').value = binaryintegral+'.'+binaryfractional;
+	else
+		document.getElementById('binarynum').value = binaryintegral;
+	document.getElementById('sign').value = sign;	
+	document.getElementById('bias').value = bias;	
+	document.getElementById('max').innerHTML = max[expolength-1];
+	document.getElementById('min').innerHTML = -max[expolength-1];
+	document.getElementById('positiveRange').innerHTML=bias;
+	document.getElementById('negativeRange').innerHTML=-bias;	
+	//The case where number is greater than 1.The exponent is always greater than zero.
+	if(number >= 1) {
+			normalized = (binaryintegral.toString()).substring(0,1) + '.';
+		for (var i=0 ; i < fraclength ; i++) {
+			if(i <=((binaryintegral.toString()).length-2)) {
+		 		normalized = normalized + (binaryintegral.toString()).substring(i+1,i+2);
+			}else{
+				normalized = normalized + binaryfractional.substring(i-binaryintegral.toString().length,i-binaryintegral.toString().length+1);
+			}
+		
+		}
+		//adding zeros to the end of the normalized number if the 
+		//length is less than 8-bit.
+		for (var i=normalized.length;i<=fraclength;i++){
+			normalized = normalized+'0';
+		}
+		twopower = (binaryintegral.toString()).length-1;
+		normalized=normalized+" X 2^("+twopower+")";
+		document.getElementById('normalized').value = normalized;
+		} else {
+			//The case where the number is below 1.Then the exponent can be zero
+		var firstone;
+		for( var i = 2;i < binarystr.length; i++) {
+			if(binarystr[i]=='1'){
+				firstone=i;
+				break;
+			}
+		}
+		normalized='1.';
+		for (var i=1; i < fraclength; i++) {
+			normalized=normalized+binarystr.substring(firstone+i,firstone+i+1);
+		}
+		for (var i=normalized.length;i<=fraclength;i++){
+			normalized = normalized+'0';
+		}
+		twopower = 1 - firstone;
+		normalized = normalized + " X 2^("+twopower+")";
+		document.getElementById('normalized').value = normalized;	
+		
+	}
+	expone = bias + twopower;
+	if ( expone < 0 ) {
+		exponebinary='0';
+		exponeunderflow = 1;
+	} else {
+		exponebinary = expone.toString(2);
+	}
+	if ( exponebinary.length > expolength ) {
+		exponeoverflow = 1;
+	}
+	for ( var i = exponebinary.length ; i < expolength ; i++) {
+		exponebinary = '0' + exponebinary;
+	}	
+	mantiss = normalized.substring(2 , fraclength+1);
+	if ( mantiss.length < (binarystr.length - (binaryintegral.toString()).length-1) ) {
+		mantissaerror=1;
+	}
+
+	document.getElementById('mantiss').value=mantiss; 
+	document.getElementById('expone').value=expone;
+	var bit8 = document.getElementById('bit8').rows[0];
+	bit8.cells[0].innerHTML=sign;
+	for ( var i = 1 ; i <=expolength ; i++ ) {
+		bit8.cells[i].innerHTML = exponebinary[i-1];
+		bit8.cells[i].style.color='blue';
+	}
+	for ( var i = expolength+1; i < 8 ; i++ ) {
+		bit8.cells[i].innerHTML = mantiss[i-expolength-1];
+		bit8.cells[i].style.color='red';
+	} 
+	//Checking for errors.
+	if(exponeoverflow||exponeunderflow||mantissaerror){
+		document.getElementById('error0').innerHTML='cannot';
+		if(exponeoverflow)
+			document.getElementById('error2').innerHTML='expone overflow';
+		if(exponeunderflow)
+			document.getElementById('error2').innerHTML='expone underflow';
+		if(mantissaerror)
+			document.getElementById('error1').innerHTML='Mantissa too small to represent the given number.'
+	} else {
+		document.getElementById('error0').innerHTML='can';
+	}
+	
+}   

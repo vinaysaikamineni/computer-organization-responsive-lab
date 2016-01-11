be1e24e93005f071eec4968b21c6c301084918fa
S Giritheja <s.giritheja@gmail.com>
2016-01-11 21:48:01 +0530

    Added Instruction box and Control Box.

--- exp9/main.js
+++ exp9/main.js
@@ -13,13 +13,78 @@ var Rttrig=0;
 var Rdtrig=0;
 var funct;
 var instr;
+var text;
+var sum;			
+var instructionCounter=0;
+var instructions = ['Write Instructions<br/> Press Load button.','Instructions Loaded<br/> Click Execute button.','Program Counter Updated <br/>Click Next button.','Instruction Fetched <br/>Click Next button.',null,'Control Signals Generated.',null,null,null,null];
+var instruction = document.getElementById('instructions');
+instruction.innerHTML=instructions[instructionCounter];
+function updatereg() {
+	var index=0;
+	$('#regmemory').find('td').each(function(){
+		r[index++]=$(this).find("input").val();
+	});
+	
+}
 function change () {
-		if(imgnumber==10)
+		if(imgnumber==10){
 			imgnumber=1;
-		img.src="frames/cpu"+imgnumber+".jpg";
-		image.src="frames/frame"+imgnumber+".mp4";
-		video.load();
-		video.play();
+			instructionCounter=0;
+		}
+		instructionCounter++;
+		//img.src="frames/cpu"+imgnumber+".jpg";
+		//image.src="frames/frame"+imgnumber+".mp4";
+		if(instructions[instructionCounter]){
+		instruction.innerHTML=instructions[instructionCounter];
+		} else {
+			if (instructionCounter==4) {
+				 	text = 'Instruction decoded. R-Type Instruction <br/> Rs = '+parseInt(Rs,2);
+					text = text + '<br/> Rd = ' + parseInt(Rd,2);
+					text = text + '<br/>Rt = ' + parseInt(Rt,2);
+					text = text + '<br/> Imm = 2080'//Need to change this depending on the input.
+					instruction.innerHTML=text;
+			}
+			if(instructionCounter==6) {
+					text = 'Instruction Execute.<br/>';
+					text = text + '<br/>BusA = ' + r[parseInt(Rs,2)];
+					text = text + '<br/>BusB = ' + r[parseInt(Rt,2)];
+					text = text + '<br/>Rw = ' + parseInt(Rd,2);
+					text = text + '<br/>Extender = 2080';//Need to change this.
+					instruction.innerHTML=text;
+			}
+			if(instructionCounter==7) {
+					text = 'ALU  Operation.<br/>';
+					text = text + '<br/>Operation = ' + instr;
+					text = text + '<br/>ALUin1 = ' + r[parseInt(Rs,2)];
+					text = text + '<br/>ALUin2 = ' + r[parseInt(Rt,2)]; 
+					instruction.innerHTML=text;
+			}
+			if(instructionCounter==8) {
+					text = 'Memory Read.<br/>';
+					text = text + '<br/>ALUout = ' + sum;
+					text = text + '<br/>Zero Flag = ' + (sum?0:1);
+					text = text + '<br/>Address = ' + sum;
+					instruction.innerHTML=text;
+			}
+			if(instructionCounter==9) {
+					text = 'Memory Write.<br/>';
+					text = text + '<br/>DataIn = ' + r[parseInt(Rt,2)];
+					text = text + '<br/>Mem Address = ' + sum;
+					text = text + '<br/>BusW = ' + sum;
+					text = text + '<br/>Rw =' + parseInt(Rd,2);
+					instruction.innerHTML=text;
+					r[parseInt(Rd,2)]=sum;
+					var index=0;
+					$('#regmemory').find('td').each(function(){
+					if(index==parseInt(Rd,2))
+					$(this).find("input").val(sum);
+					index++;
+					});
+	
+			}
+		}
+		//video.load();
+		//video.play();
 		imgnumber++;
 	}
 
@@ -28,22 +93,34 @@ function change () {
 		console.log(result);
 		result.rows[1].cells[1].innerHTML=Rs;	
 		result.rows[1].cells[2].innerHTML=Rt;
-		result.rows[1].cells[3].innerHTML=Rd;
 		if(instr=="add"){
 		result.rows[1].cells[5].innerHTML="10000";	
+		sum=parseInt(r[parseInt(Rs,2)])+parseInt(r[parseInt(Rt,2)]);
 		}
 		if(instr=="and"||instr=="or"){
-		result.rows[1].cells[3].innerHTML="10010";	
+		result.rows[1].cells[5].innerHTML="10010";	
+		if(instr == "and") {
+			sum=parseInt(r[parseInt(Rs,2)])&parseInt(r[parseInt(Rt,2)]);
+		}
+		if(instr == "or") {
+			sum=parseInt(r[parseInt(Rs,2)])|parseInt(r[parseInt(Rt,2)]);
+
+		}
 		}
 		if(instr=="sub"){
-		result.rows[1].cells[3].innerHTML="10001";	
+		result.rows[1].cells[5].innerHTML="10001";	
+		sum=parseInt(r[parseInt(Rs,2)])-parseInt(r[parseInt(Rt,2)]);
 		}
+		document.getElementById('aluctr').innerHTML = instr;
+				
 	}
 function load() {
 		var rowname='row'+rownumber;
 		var inputs=document.getElementById(rowname).value;
 		inputs = inputs.split(/[\s,]+/);
 		console.log(inputs);
+		instructionCounter++;
+		instruction.innerHTML=instructions[instructionCounter];
 		for(index in inputs){
 			if(inputs[index] == "and"){
 				instr="and";			
